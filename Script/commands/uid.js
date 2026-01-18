b69af636eb492967d990ff25333a199e1edad265
rafi69691 <akramhassion5136@gmail.com>
2026-01-18 21:17:36 +0600

    Update uid.js

--- Script/commands/uid.js
+++ Script/commands/uid.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "uid",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "RAFI BOT",
 	description: "Get User ID.",
 	commandCategory: "Tools",
 	cooldowns: 5
