5d2b46d64a62b844feb0b49d7642ca9eeacd8fea
rafi69691 <akramhassion5136@gmail.com>
2026-01-18 21:11:34 +0600

    Update Playstor.js

--- Script/commands/Playstor.js
+++ Script/commands/Playstor.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "playstore",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  credits: "RAFI BOT",
   description: "",
   commandCategory: "User",
   usages: "",
