a29998ba6ab2625fac52480e4bbd675c7f30cb0a
rafi69691 <akramhassion5136@gmail.com>
2026-01-18 21:19:57 +0600

    Update unban.js

--- Script/commands/unban.js
+++ Script/commands/unban.js
@@ -2,7 +2,7 @@ module.exports.config = {
  name: "unban",
  version: "1.0.0",
  hasPermssion: 2,
- credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+ credits: "RAFI BOT",
  description: "Remove groups and users in 1 note",
  commandCategory: "Admin",
  usages: "unban",
