264ba0a02fed905b0dc15910a5a2b6b762ca969b
rafi69691 <akramhassion5136@gmail.com>
2026-01-18 21:20:34 +0600

    Update unban.js

--- Script/commands/unban.js
+++ Script/commands/unban.js
@@ -16,7 +16,7 @@ module.exports.run = async ({ event, api, Users, Threads, args }) => {
  const { commands } = global.client;
  const command = commands.get(("unban").toLowerCase());
  const credit = command.config.credits;
- var mangG = "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️";
+ var mangG = "RAFI BOT";
  if(credit != mangG) return api.sendMessage(`Sai credit!`, event.threadID, event.messageID);
  
  const threadSetting = global.data.threadData.get(parseInt(event.threadID)) || {};
